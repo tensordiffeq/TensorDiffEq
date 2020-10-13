76867fcde69d8e98f7fe43b89c2f1e0417020caa
Levi <levimcclenny@tamu.edu>
2020-10-12 20:14:08 -0500

    Initial push from macbook

---
+++ tensordiffeq/networks.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras import layers, activations
+
+#define the neural network model
+def neural_net(layer_sizes):
+    model = Sequential()
+    model.add(layers.InputLayer(input_shape=(layer_sizes[0],)))
+    for width in layer_sizes[1:-1]:
+        model.add(layers.Dense(
+            width, activation=tf.nn.tanh,
+            kernel_initializer="glorot_normal"))
+    model.add(layers.Dense(
+            layer_sizes[-1], activation=None,
+            kernel_initializer="glorot_normal"))
+    return model
---
+++ tensordiffeq/utils.py
@@ -0,0 +1,26 @@
+
+import tensorflow as tf
+
+def set_weights(self, model, w, sizes_w, sizes_b):
+        for i, layer in enumerate(model.layers[0:]):
+            start_weights = sum(sizes_w[:i]) + sum(sizes_b[:i])
+            end_weights = sum(sizes_w[:i+1]) + sum(sizes_b[:i])
+            weights = w[start_weights:end_weights]
+            w_div = int(sizes_w[i] / sizes_b[i])
+            weights = tf.reshape(weights, [w_div, sizes_b[i]])
+            biases = w[end_weights:end_weights + sizes_b[i]]
+            weights_biases = [weights, biases]
+            layer.set_weights(weights_biases)
+
+
+def get_weights(model):
+        w = []
+        for layer in model.layers[0:]:
+            weights_biases = layer.get_weights()
+            weights = weights_biases[0].flatten()
+            biases = weights_biases[1]
+            w.extend(weights)
+            w.extend(biases)
+
+        w = tf.convert_to_tensor(w)
+        return w
