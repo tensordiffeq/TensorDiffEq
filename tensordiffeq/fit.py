5b19949076a1d33bffc8da2bd1b4345752e80a4d
Levi McClenny <levimcclenny@tamu.edu>
2020-11-03 08:15:50 -0600

    Commit fit progress

--- tensordiffeq/fit.py
+++ tensordiffeq/fit.py
@@ -51,11 +51,15 @@ def fit(obj, tf_iter, newton_iter, batch_sz = None):
       Struct(), maxIter=newton_iter, learningRate=0.8)
 
 
+
 def fit_dist(obj, tf_iter, newton_iter, batch_sz = None):
+
+    BUFFER_SIZE = len(obj.x_f)
+    EPOCHS = tf_iter
+
     strategy = tf.distribute.MirroredStrategy()
     print("number of devices: {}".format(strategy.num_replicas_in_sync))
-    obj.u_model = neural_net(obj.layer_sizes)
-    #Can adjust batch size for collocation points, here we set it to N_f
+
     if batch_sz is not None:
         obj.batch_sz = batch_sz
     else:
@@ -64,9 +68,20 @@ def fit_dist(obj, tf_iter, newton_iter, batch_sz = None):
     N_f = len(obj.x_f)
     n_batches =  N_f // obj.batch_sz
 
+    BATCH_SIZE_PER_REPLICA = obj.batch_sz
+    GLOBAL_BATCH_SIZE = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
+
+    train_dataset = tf.data.Dataset.from_tensor_slices((self.x_f, self.t_f))
+
     start_time = time.time()
-    tf_optimizer = tf.keras.optimizers.Adam(lr = 0.005, beta_1=.99)
-    tf_optimizer_weights = tf.keras.optimizers.Adam(lr = 0.005, beta_1=.99)
+
+    with strategy.scope():
+        obj.u_model = neural_net(obj.layer_sizes)
+        tf_optimizer = tf.keras.optimizers.Adam(lr = 0.005, beta_1=.99)
+        tf_optimizer_weights = tf.keras.optimizers.Adam(lr = 0.005, beta_1=.99)
+        #Can adjust batch size for collocation points, here we set it to N_f
+
+
     #tf_optimizer_u = tf.keras.optimizers.Adam(lr = 0.005, beta_1=.99)
 
     print("starting Adam training")
@@ -95,3 +110,15 @@ def fit_dist(obj, tf_iter, newton_iter, batch_sz = None):
     lbfgs(loss_and_flat_grad,
       get_weights(obj.u_model),
       Struct(), maxIter=newton_iter, learningRate=0.8)
+
+def train_step(inputs):
+    x_f, t_f = inputs
+
+def train_op_tf(obj):
+    if obj.isAdaptive:
+        loss_value, mse_0, mse_b, mse_f, grads, grads_col, grads_u = obj.adaptgrad()
+        tf_optimizer.apply_gradients(zip(grads, obj.u_model.trainable_variables))
+        tf_optimizer_weights.apply_gradients(zip([-grads_col, -grads_u], [obj.col_weights, obj.u_weights]))
+    else:
+        loss_value, mse_0, mse_b, mse_f, grads = obj.grad()
+        tf_optimizer.apply_gradients(zip(grads, obj.u_model.trainable_variables))
