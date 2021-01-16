6de1619541b2e1204adf79bb7bcbfb1a447821f8
Levi McClenny <levimcclenny@tamu.edu>
2021-01-15 21:47:38 -0600

    Update model_new

--- examples/testing.py
+++ examples/testing.py
@@ -7,24 +7,24 @@ from tensordiffeq.models_new import CollocationSolver1D
 from tensordiffeq.domains import DomainND
 from tensordiffeq.boundaries import *
 
-Domain = DomainND(["x", 'y', 'z', "t"])
+Domain = DomainND(["x", "t"])
 
 Domain.add("x", [-1.0,1.0], 512)
 Domain.add("t", [0.0,1.0], 100)
 
+def func_ic(x):
+    return np.sin(x*math.pi)
 
+init = IC(Domain, func_ic, vars = 'x')
 upper_x = dirichlectBC(Domain, val = 0.0, var = 'x',target = "upper")
 lower_x = dirichlectBC(Domain, val = 0.0, var = 'x',target = "lower")
 
-
-
-print(upper_x.out)
-
-
-
+BCs = [init, upper_x,  lower_x]
 
 def f_model(u_model, inputs):
-    u = u_model(tf.concat([x,t],1))
+    x = tf.gather(inputs, [0])
+    t = tf.gather(inputs, [1])
+    u = u_model(inputs)
     u_x = tf.gradients(u, x)
     u_xx = tf.gradients(u_x, x)
     u_t = tf.gradients(u, t)
@@ -32,3 +32,7 @@ def f_model(u_model, inputs):
     f_u = u_t + u*u_x - (0.05/tf.constant(math.pi))*u_xx
 
     return f_u
+
+model = CollocationSolverND()
+
+model.fit(Domain, BCs, f_model, ...)
--- tensordiffeq/models_new.py
+++ tensordiffeq/models_new.py
@@ -7,7 +7,7 @@ from .networks import *
 from .plotting import *
 from .fit import *
 
-class CollocationSolver1D():
+class CollocationSolverND():
     def __init__(self, assimilate = False):
         self.assimilate = assimilate
         self.periodicBC = False
