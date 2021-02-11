4f767be591bf693d19211ad8f3eac7249d6ec8c2
Levi McClenny <levimcclenny@tamu.edu>
2021-02-10 18:32:36 -0600

    v0.1.5 fix install

---
+++ tensordiffeq/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+
+from tensordiffeq import models, optimizers, networks, plotting, utils, domains, boundaries, fit, helpers
+
+# from .models import CollocationSolverND, DiscoveryModel
+# from .boundaries import dirichletBC, periodicBC, IC
+# from .utils import constant, LatinHypercubeSample, tensor
+# from .plotting import newfig, get_griddata
+# from .helpers import find_L2_error
+# from .optimizers import graph_lbfgs, eager_lbfgs
+
+
+__all__ = [
+    "models",
+    "networks",
+    "plotting",
+    "utils",
+    "helpers",
+    "optimizers",
+    "boundaries",
+    "domains",
+    "fit",
+]
