9a1303a61d68c06605ee3562c5e005a89265da60
Levi McClenny <levimcclenny@tamu.edu>
2021-03-27 18:52:34 -0500

    stage v0.1.7

--- tensordiffeq/output.py
+++ tensordiffeq/output.py
@@ -2,6 +2,8 @@ from pyfiglet import Figlet
 from os import system, name
 import sys
 
-def print_screen(Domain, model):
+def print_screen(model):
     f = Figlet(font='slant')
     print(f.renderText('TensorDiffEq'))
+    print("Neural Network Model Summary\n")
+    print(model.u_model.summary())
